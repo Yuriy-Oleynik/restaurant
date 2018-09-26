dd41df863d44b7605c7ccd6a9d61aea46bdd36c7
Юрий <yurikua11@gmail.com>
2018-09-26 11:28:42 +0300

    add classes in directory: model, repository, service

--- src/main/java/repository/MealRepository.java
+++ src/main/java/repository/MealRepository.java
@@ -1,5 +1,5 @@
 package repository;
 
-public class MealRepository {
+public interface MealRepository {
 
 }
--- src/main/java/service/MealService.java
+++ src/main/java/service/MealService.java
@@ -1,5 +1,5 @@
 package service;
 
-public class MealService {
+public interface MealService {
 
 }
