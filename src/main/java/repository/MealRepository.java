79ba09ad0d315010cdd738ad56fd965defc25ab3
Юрий <yurikua11@gmail.com>
2018-09-29 20:11:34 +0300

    add classes in directory: exception

--- src/main/java/repository/MealRepository.java
+++ src/main/java/repository/MealRepository.java
@@ -1,5 +1,41 @@
 package repository;
 
-public interface MealRepository {
+import model.Meal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import util.exception.NotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MealRepository extends JpaRepository<Meal, Integer> {
+
+    @Override
+    List<Meal> findAll();
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Override
+    Optional<Meal> findById(Integer id);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id")
+    void delete(@Param("id") int id) throws NotFoundException;
+
+    List<Meal> getAllByRestaurantId(Integer id);
+
+    @Query("SELECT m FROM Meal m WHERE m.date >= CURRENT_DATE AND m.restaurant.id=:id")
+    List<Meal> getAllForTodayByRestaurantId(@Param("id") Integer id);
+
+    @Query("SELECT SUM (m.price) FROM Meal m WHERE m.date>=CURRENT_DATE AND m.restaurant.id=:id")
+    Integer getMealsPriceForTodayByRestaurantId(@Param("id") Integer id);
 
 }
